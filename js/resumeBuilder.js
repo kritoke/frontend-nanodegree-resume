cf11b80781cce0b5d011b8b55a2b7232a2308c14
Matt Rhone <kritoke@gmail.com>
2017-11-19 12:12:49 -0600

    Start initial building of bio and bioview

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,32 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// bio model
+var bio = {
+    name: 'Matthew Rhone',
+    role: 'IT Professional',
+    contacts: {
+        mobile: '867-5301',
+        email: 'notarealemail@somewhere.com',
+        github: 'https://github.com/kritoke',
+        twitter: 'kritoke',
+        location: 'Lake Charles'
+    },
+    welcomeMessage: '',
+    skills: ['Linux', 'JavaScript', 'Ruby', 'Windows Server Support', 'Desktop Support', 'Graphics Design'],
+    biopic: 'images/fry.jpg',
+    display: function() {
+        bioView.render();
+    }
+}
+
+var bioView = {
+    init: function() {
+        this.render();
+    },
+
+    render() {
+
+    }
+}
